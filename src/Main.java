df30eb6a2be0c0b0eeae1889e495d8714e8c4234
alperen010010 <bircakalperen@gmail.com>
2022-11-16 17:25:55 +0300

    connection test

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,40 @@
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+import static java.net.InetAddress.getLocalHost;
+
 public class Main {
+    static int port = 8080;
+    static String hostname = "localhost";
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            InetAddress localhost = getLocalHost();
+            System.out.println(localhost.getHostAddress());
+
+            Socket s = new Socket(hostname, port);
+
+            System.out.println("connected");
+
+            Scanner in = new Scanner(System.in);
+
+            DataOutputStream output = new DataOutputStream(s.getOutputStream());
+
+            while(true) {
+                String line = in.nextLine();
+                output.writeBytes(line+"\n");
+                System.out.println("You sent string " + line);
+            }
+
+
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+
     }
 }
