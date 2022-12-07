16e4f877cdf252b16a6d7c1ecd02677ebb452231
alperen010010 <bircakalperen@gmail.com>
2022-12-07 22:12:14 +0300

    NBR request test

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
+import java.net.ConnectException;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -10,27 +9,54 @@ import static java.net.InetAddress.getLocalHost;
 
 public class Main {
     static int port = 8080;
-    static String hostname = "192.168.1.58";
+    static String hostname = "localhost";
+
+    static String test = """
+            3#
+            route:test;
+            name:Alperen;
+            surname:Bircak;
+            """;
 
     public static void main(String[] args) {
         try {
+            System.out.println(hostname + ':' + port);
+
+            while (true) {
+                Scanner input = new Scanner(System.in);
+                try {
+                    Socket s = new Socket(hostname, port);
+                    System.out.println("Connection Established.");
+
+                    DataOutputStream out = new DataOutputStream(s.getOutputStream());
+
+                    out.writeBytes(test);
+
+
+                    //System.out.println(s.isClosed());
+
+
+                    BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+                    String line = "";
+                    while ((line = in.readLine()) != null) {
+                        System.out.println(line);
+
+                    }
 
 
 
-            while(true) {
-                Scanner in = new Scanner(System.in);
-                String line = in.nextLine();
+                    System.out.println("end...");
+                    Thread.sleep(2000);
+                } catch (ConnectException e) {
+                    System.out.println("No connection trying again...");
+                    Thread.sleep(2000);
+                }
 
-                Socket s = new Socket(hostname, port);
-                DataOutputStream output = new DataOutputStream(s.getOutputStream());
-                output.writeBytes(line+"\n");
-                System.out.println("You sent string " + line);
-                s.close();
             }
-
-
         } catch (IOException e) {
             System.out.println(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
 
     }
