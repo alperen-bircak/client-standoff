ea2dc127affe80445a90a550a26a77fcb13314e6
alperen010010 <bircakalperen@gmail.com>
2022-12-20 20:56:37 +0300

    change

--- src/Main.java
+++ src/Main.java
@@ -9,13 +9,33 @@ import static java.net.InetAddress.getLocalHost;
 
 public class Main {
     static int port = 8080;
-    static String hostname = "18.204.213.255";
+    static String hostname = "localhost"; //18.204.213.255
 
-    static String test = """
+    static String test5 = """
+            2#
+            route:game.close;
+            game_id:91bb5698-de0a-4753-b488-16896a1a6f1b;
+            """;
+    static String test4 = """
+            2#
+            route:game.state;
+            game_id:91bb5698-de0a-4753-b488-16896a1a6f1b;
+            """;
+    static String test3 = """
             3#
-            route:test;
+            route:game.enter;
+            key:ODFA;
+            player_id:a73c5407-980c-439e-82cc-04ea6c0fa369;
+            """;
+    static String test2 = """
+            2#
+            route:game.start;
+            player_id:a73c5407-980c-439e-82cc-04ea6c0fa369;
+            """;
+    static String test1 = """
+            2#
+            route:player.add;
             name:Alperen;
-            surname:Bircak;
             """;
 
     public static void main(String[] args) {
@@ -30,7 +50,7 @@ public class Main {
 
                     DataOutputStream out = new DataOutputStream(s.getOutputStream());
 
-                    out.writeBytes(test);
+                    out.writeBytes(test5);
 
 
                     //System.out.println(s.isClosed());
@@ -46,7 +66,7 @@ public class Main {
 
 
                     System.out.println("end...");
-                    Thread.sleep(2000);
+                    break;
                 } catch (ConnectException e) {
                     System.out.println("No connection trying again...");
                     Thread.sleep(2000);
